9770447e7d1cc0a229dd859527849146167d3814
Bernd Eckenfels <bernd@eckenfels.net>
2014-02-13 02:46:02 +0100

    Add a simple test Mojo - project based on plugin-archetype.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ lockdep-maven-plugin
 ====================
 
 Lock Maven Dependencies by cryptographic hashes
+
+This is work in progress, currently it only builds a
+simple maven plugin which logs the dependencies found.
---
+++ src/main/java/net/eckenfels/mavenplugins/lockdeps/LockDependenciesMojo.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2014 Bernd Eckenfels, Germany.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ */
+package net.eckenfels.mavenplugins.lockdeps;
+
+import java.io.File;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+
+@Mojo( name = "lockdeps", defaultPhase = LifecyclePhase.VERIFY, requiresDependencyCollection=ResolutionScope.TEST )
+public class LockDependenciesMojo
+    extends AbstractMojo
+{
+    @Parameter( defaultValue = "${project}", required = true, readonly = true )
+    protected MavenProject project;
+
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        Log log = getLog();
+
+        log.debug("Now scanning dependencies..." + project);
+
+        Set<Artifact> deps = project.getArtifacts();
+        for(Artifact a : deps)
+        {
+            log.info("artifact: " + a.getId() + " (" + a.getScope() +") from " +  a.getDownloadUrl() + " " + a.getFile());
+        }
+
+        deps = project.getPluginArtifacts();
+        for(Artifact a : deps)
+        {
+            a.
+            log.info("plugin artifact: " + a.getId() + " (" + a.getScope() +") from " +  a.getDownloadUrl() + " " + a.getFile());
+        }
+    }
+}
